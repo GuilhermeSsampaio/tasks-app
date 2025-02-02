6d05e4ef64cecd1ff6fd8f85f2d8065aee0cc43c
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2025-02-01 22:03:33 -0400

    corrigindo

--- pages/api/jobs.js
+++ pages/api/jobs.js
@@ -2,6 +2,7 @@ import supabase from "@/DB/db";
 
 export default async function handleJobs(req, res) {
   if (req.method === "GET") {
+    // Recuperar jobs do banco de dados
     const { data, error } = await supabase.from("jobs").select("*");
 
     if (error) {
@@ -10,6 +11,7 @@ export default async function handleJobs(req, res) {
 
     return res.status(200).json(data);
   } else if (req.method === "POST") {
+    // Adicionar um novo job ao banco de dados
     const { label, checked } = req.body;
     const { data, error } = await supabase
       .from("jobs")
@@ -18,26 +20,34 @@ export default async function handleJobs(req, res) {
     if (error) {
       return res.status(500).json({ error: error.message });
     }
+
     return res.status(201).json(data);
-  } else if (req.method === "DELETE") {
-    const { id } = req.query;
-    const { data, error } = await supabase.from("jobs").delete().eq("id", id);
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
-    return res.status(200).json(data);
   } else if (req.method === "PUT") {
-    const { id, label } = req.query;
+    // Atualizar um job no banco de dados
+    const { id } = req.query;
+    const { label } = req.body;
     const { data, error } = await supabase
       .from("jobs")
-      .update([{ label }])
+      .update({ label })
       .eq("id", id);
+
     if (error) {
       return res.status(500).json({ error: error.message });
     }
+
+    return res.status(200).json(data);
+  } else if (req.method === "DELETE") {
+    // Deletar um job do banco de dados
+    const { id } = req.query;
+    const { data, error } = await supabase.from("jobs").delete().eq("id", id);
+
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+
     return res.status(200).json(data);
   } else {
-    res.setHeader("Allow", ["GET", "POST", "DELETE"]);
-    res.status(405).end(`Method ${req.method} Not allowed`);
+    res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
